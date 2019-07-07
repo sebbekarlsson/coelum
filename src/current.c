3f66670c6d8cfb3e74edbc891e602d6c4192ef45
ianertson <sebbekarlsson97@gmail.com>
2019-07-07 20:17:01 +0200

    add simple helper function to get current scene

---
+++ src/current.c
@@ -0,0 +1,10 @@
+#include "include/current.h"
+
+
+extern theatre_T* THEATRE;
+
+
+scene_T* get_current_scene()
+{
+    return scene_manager_get_current_scene(THEATRE->scene_manager);
+}
---
+++ src/include/current.h
@@ -0,0 +1,7 @@
+#ifndef COELUM_CURRENT_H
+#define COELUM_CURRENT_H
+#include "theatre.h"
+
+
+scene_T* get_current_scene();
+#endif
