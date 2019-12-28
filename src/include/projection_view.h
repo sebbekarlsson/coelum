a3aee352bfbf556cec94407dbfed427b5b55df20
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2019-12-28 23:25:57 +0100

    be able to set fov on projection_view

--- src/include/projection_view.h
+++ src/include/projection_view.h
@@ -8,12 +8,17 @@ typedef struct PROJECTION_VIEW_STRUCT
 {
     mat4 projection;
     mat4 view;
+    int width;
+    int height;
     unsigned int dimensions;
+    float fov; 
 } projection_view_T;
 
 projection_view_T* init_projection_view(int width, int height, unsigned int dimensions);
 
 void projection_view_translate(projection_view_T* projection_view, float x, float y, float z);
+
+void projection_view_recalculate_projection(projection_view_T* projection_view);
 
 void projection_view_free(projection_view_T* projection_view);
 #endif
--- src/projection_view.c
+++ src/projection_view.c
@@ -18,6 +18,9 @@ projection_view_T* init_projection_view(int width, int height, unsigned int dime
 {
     projection_view_T* pv = calloc(1, sizeof(struct PROJECTION_VIEW_STRUCT));
     pv->dimensions = dimensions;
+    pv->width = width;
+    pv->height = height;
+    pv->fov = 45.0f;
 
     mat4 view =
     {
@@ -39,21 +42,8 @@ projection_view_T* init_projection_view(int width, int height, unsigned int dime
     memcpy(pv->projection, projection, sizeof(projection));
 
     projection_view_translate(pv, 0.0f, 0.0f, 0.0f);
+    projection_view_recalculate_projection(pv);
 
-    if (pv->dimensions == 2)
-    { // we are doing 2D
-        glm_ortho(0.0f, width, height, 0.0f, -10.0f, 10.0f, pv->projection);
-    }
-    else
-    { // we are doing 3D
-        glm_perspective(glm_rad(45.0f), (float) width / (float)height, 0.01f, 1000.0f, pv->projection);
-        /*glm_lookat(
-            (vec3){0, 0, 0},
-            (vec3){0, 0, 0},
-            (vec3){0, 1, 0},
-            pv->view
-        );*/
-    } 
     return pv;
 }
 
@@ -68,6 +58,23 @@ projection_view_T* init_projection_view(int width, int height, unsigned int dime
 void projection_view_translate(projection_view_T* projection_view, float x, float y, float z)
 {
     glm_translate(projection_view->view, (vec3){x, y, z});
+}
+
+/**
+ * Recalculate the projection matrix of a projection_view
+ *
+ * @param projection_view_T* projection_view
+ */
+void projection_view_recalculate_projection(projection_view_T* projection_view)
+{
+    if (projection_view->dimensions == 2)
+    { // we are doing 2D
+        glm_ortho(0.0f, projection_view->width, projection_view->height, 0.0f, -10.0f, 10.0f, projection_view->projection);
+    }
+    else
+    { // we are doing 3D
+        glm_perspective(glm_rad(projection_view->fov), (float) projection_view->width / (float) projection_view->height, 0.01f, 1000.0f, projection_view->projection);
+    }
 }
 
 /**
