8b32151f4de0c60d5284f097b5e34bb9040c7a04
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-01-01 21:29:18 +0100

    implements frustum culling with glm functions

--- src/frustum.c
+++ src/frustum.c
@@ -2,61 +2,23 @@
 #include <string.h>
 
 
-frustum_plane_T* init_frustum_plane()
-{
-    frustum_plane_T* plane = calloc(1, sizeof(struct FRUSTUM_PLANE_STRUCT));
-    plane->distance = 0.0f;
-    
-    return plane;
-}
-
-void frustum_plane_set_normal_and_position(frustum_plane_T* plane, vec3 normal, vec3 position)
-{
-    glm_vec3_copy(normal, plane->normal);
-    glm_vec3_copy(position, plane->position);
-}
-
-float frustum_plane_distance_to_point(frustum_plane_T* plane, vec3 position)
-{
-
-}
-
 frustum_T* init_frustum()
 {
     frustum_T* frustum = calloc(1, sizeof(struct FRUSTUM_STRUCT));
-
-    // near plane
-    frustum->planes[0] = init_frustum_plane((vec3){0.0f, 0.0f, 1.0f});
-
-    // far plane
-    frustum->planes[1] = init_frustum_plane((vec3){0.0f, 0.0f, -1.0f});
-    
-    // left plane
-    frustum->planes[2] = init_frustum_plane((vec3){-1.0f, 0.0f, 0.0f});
-
-    // right plane
-    frustum->planes[3] = init_frustum_plane((vec3){1.0f, 0.0f, 0.0f});
-
-    // top plane
-    frustum->planes[4] = init_frustum_plane((vec3){0.0f, 1.0f, 0.0f});
-
-    // bottom plane
-    frustum->planes[5] = init_frustum_plane((vec3){0.0f, -1.0f, 0.0f});
+    return frustum;
 }
 
-void frustum_recalc_geometry(float near, float far, float ratio, float angle, vec3 camera_position)
+void frustum_recalc_geometry(frustum_T* frustum, mat4 projection, mat4 view)
 {
-}
+    //mat4 invViewProj;
 
-unsigned int frustum_is_point_within(frustum_T* frustum, vec3 position)
-{
-	for(int i=0; i < 6; i++)
-    {
-        frustum_plane_T* plane = frustum->planes[i];
+    //glm_mat4_inv(cam->viewProj, invViewProj);
+    mat4 viewProj, invViewProj;
+    glm_mat4_mul(projection, view, viewProj);
 
-		if (frustum_plane_distance_to_point(frustum, position) < 0)
-			return 0;
-	}
+    glm_frustum_planes(viewProj,        frustum->planes);
 
-	return 1;
+    glm_mat4_inv(viewProj, invViewProj);
+    glm_frustum_corners(invViewProj,         frustum->corners);
+    glm_frustum_center(frustum->corners, frustum->center);
 }
--- src/include/frustum.h
+++ src/include/frustum.h
@@ -3,27 +3,14 @@
 #include <cglm/cglm.h>
 
 
-typedef struct FRUSTUM_PLANE_STRUCT
-{
-    vec3 normal;
-    vec3 position;
-    float distance;
-} frustum_plane_T;
-
-frustum_plane_T* init_frustum_plane();
-
-void frustum_plane_set_normal_and_position(frustum_plane_T* plane, vec3 normal, vec3 position);
-
-float frustum_plane_distance_to_point(frustum_plane_T* plane, vec3 position);
-
 typedef struct FRUSTUM_STRUCT
 {
-    frustum_plane_T* planes[6];
+    vec4 planes[6];
+    vec4 corners[8];
+    vec4 center;
 } frustum_T;
 
 frustum_T* init_frustum();
 
-void frustum_recalc_geometry(float near, float far, float ratio, float angle, vec3 camera_position);
-
-unsigned int frustum_is_point_within(frustum_T* frustum, vec3 position);
+void frustum_recalc_geometry(frustum_T* frustum, mat4 projection, mat4 view);
 #endif
